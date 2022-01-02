e9be324006da1e3227897c98591afb7cf09e4f14
Johnny Lo <klloaj@connect.ust.hk>
2022-01-02 16:16:34 +0800

    added valid voter list

--- blockchain/votingtest/include/votingtest.hpp
+++ blockchain/votingtest/include/votingtest.hpp
@@ -7,9 +7,11 @@ CONTRACT votingtest : public contract {
  public:
   using contract::contract;
 
+  ACTION addvoter(name new_voter);
+
   ACTION addcandidate(name new_candidate);
-  ACTION vote(name candidate);
-  ACTION unvote(name candidate);
+  ACTION vote(name voter, name candidate);
+  ACTION unvote(name voter, name candidate);
   ACTION clear();
 
   TABLE voting_result {
@@ -17,5 +19,13 @@ CONTRACT votingtest : public contract {
     uint64_t vote_count;
     auto primary_key() const { return candidate.value; }
   };
+
+  TABLE voter_list {
+    name voter;
+    auto primary_key() const { return voter.value; }
+  };
+
   typedef multi_index<name("votingresult"), voting_result> voting_result_table;
+
+  typedef multi_index<name("voterlist"), voter_list> voter_list_table;
 };
--- blockchain/votingtest/src/votingtest.cpp
+++ blockchain/votingtest/src/votingtest.cpp
@@ -11,7 +11,23 @@ ACTION votingtest::addcandidate(name new_candidate) {
   });
 }
 
-ACTION votingtest::vote(name candidate) {
+ACTION votingtest::addvoter(name new_voter) {
+  require_auth(get_self());
+  voter_list_table _voter(get_self(), get_self().value);
+  auto voting_itr = _voter.find(new_voter.value);
+  check(voting_itr == _voter.end(), "voter exist");
+  _voter.emplace(get_self(), [&](auto& new_voter_record) {
+    new_voter_record.voter = new_voter;
+  });
+}
+
+ACTION votingtest::vote(name voter, name candidate) {
+  check(has_auth(voter), "You are not voter.");
+
+  voter_list_table _voter(get_self(), get_self().value);
+  auto voter_itr = _voter.find(voter.value);
+  check(voter_itr != _voter.end(), "You cannot voter");
+
   voting_result_table _voting(get_self(), get_self().value);
   auto voting_itr = _voting.find(candidate.value);
   check(voting_itr != _voting.end(), "candidate is not exist");
@@ -20,7 +36,13 @@ ACTION votingtest::vote(name candidate) {
                  [&](auto& ballot) { ++ballot.vote_count; });
 }
 
-ACTION votingtest::unvote(name candidate) {
+ACTION votingtest::unvote(name voter, name candidate) {
+  check(has_auth(voter), "You are not voter.");
+
+  voter_list_table _voter(get_self(), get_self().value);
+  auto voter_itr = _voter.find(voter.value);
+  check(voter_itr != _voter.end(), "You cannot voter");
+
   voting_result_table _voting(get_self(), get_self().value);
   auto voting_itr = _voting.find(candidate.value);
   check(voting_itr != _voting.end(), "candidate is not exist");
@@ -41,4 +63,4 @@ ACTION votingtest::clear() {
   }
 }
 
-EOSIO_DISPATCH(votingtest, (addcandidate)(vote)(clear))
+EOSIO_DISPATCH(votingtest, (addcandidate)(vote)(clear)(addvoter))
