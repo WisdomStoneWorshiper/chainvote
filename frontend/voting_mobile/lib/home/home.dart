ae1b1e20dcd562056b1eea48142b98792f3ad37c
Mudit <muditsangavi@gmail.com>
2022-02-05 01:17:00 +0530

    added temp code for home page in comments

--- frontend/voting_mobile/lib/home/home.dart
+++ frontend/voting_mobile/lib/home/home.dart
@@ -24,3 +24,18 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+// body: Center(
+//         // child: const Text(
+//         //   "We are still developing the homepage",
+//         //   style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+//         // ),
+//         child: RichText(
+//           text: const TextSpan(
+//             text: 'Home Page',
+//             style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 30.0,
+//                 fontWeight: FontWeight.bold),
+//           ),
+//         ),
+//       ),
