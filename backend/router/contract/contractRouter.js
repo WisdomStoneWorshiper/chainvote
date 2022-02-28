859d8668178804a7e288fa63b729be30f7e60cd2
Kinshuk Kotecha <kotechakinshuk@gmail.com>
2022-02-28 17:37:01 +0800

    helper functions + edit add voter

--- backend/router/contract/contractRouter.js
+++ backend/router/contract/contractRouter.js
@@ -7,39 +7,45 @@ const { addVoterPlaceholder } = require("../../Helper functions/eosPlaceholder")
 require("dotenv").config();
 
 router.post("/addvoter", async (req, res) => {
-    const { itsc, campaignId } = req.body;
-    const data = await accModel
-        .findOne({itsc : itsc})
-        .select("accountName")
-        .then(result => res.json(result));
-    
-    if(!data || !data.accountName){
-        res.status(400).json({
-            error : true,
-            message : "Cannot find accountName"
-        })
-    };
+    const { itsc_list, campaignId } = req.body;
+    for (let i = 0; i < itsc_list; i++) {
+        const data = await accModel
+            .findOne({itsc : itsc_list[i]})
+            .select("accountName")
+            .then(result => res.json(result));
+        
+        if(!data || !data.accountName){
+            res.status(400).json({
+                error : true,
+                message : "Cannot find accountName",
+                itsc : itsc_list[i]
+            })
+        };
 
-    eosDriver.transact({
-        actions : [addVoterPlaceholder(accName, campaignId)]},
-        {
-            blocksBehind: 3,
-            expireSeconds: 30,
-        }
-    )
-    .then( result => {
-        res.json({
-            error : false
-        });
-    })
-    .catch( err => {
-        res.status(400).json({
-            error : false,
-            message : err.message
+        eosDriver.transact({
+            actions : [addVoterPlaceholder(accName, campaignId)]},
+            {
+                blocksBehind: 3,
+                expireSeconds: 30,
+            }
+        )
+        .then( result => {
+            res.json({
+                error : false
+            });
         })
-    })
+        .catch( err => {
+            res.status(400).json({
+                error : true,
+                message : err.message,
+                itsc : itsc_list[i]
+            })
+        })
+    }
 
 });
+
+
 
 router.post("/login", async (req, res) => {
     const { itsc, publicKey } = req.body;
@@ -57,6 +63,51 @@ router.post("/login", async (req, res) => {
         res.status(500).json({
             error : true,
             message : err.message
+        })
+    })
+});
+
+router.post("/getITSC", async (req, res) => {
+    const { accountName } = req.body;
+    accModel.findOne({
+        accountName : accountName
+    })
+    .then( result => {
+        res.json({
+            error : false,
+            itsc : result.itsc
+        });
+    })
+    .catch( err => {
+        res.status(500).json({
+            error : true,
+            message : "This account name is not linked to any ITSC"
+        })
+    })
+});
+
+router.post("/checkPubKey", async (req, res) => {
+    const { itsc , publicKey } = req.body;
+    accModel.findOne({
+        itsc : itsc
+    })
+    .then( result => {
+        if (result.publicKey == publicKey) {
+            res.json({
+                error : false,
+                match : true
+            })
+        } else {
+            res.json({
+                error : false,
+                match : false
+            });
+        }
+    })
+    .catch( err => {
+        res.status(500).json({
+            error : true,
+            message : "Invalid ITSC"
         })
     })
 });
