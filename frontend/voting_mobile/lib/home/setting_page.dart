5d536f7b41e6430c33ec421ba66e197d161e49b8
Johnny Lo <klloaj@connect.ust.hk>
2022-03-09 20:21:30 +0800

    removed biometric

--- frontend/voting_mobile/lib/home/setting_page.dart
+++ frontend/voting_mobile/lib/home/setting_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:biometric_storage/biometric_storage.dart';
 
-class SettingPage extends StatelessWidget {
-  const SettingPage({Key? key}) : super(key: key);
+import '../shared_dialog.dart';
+
+class SettingPage extends StatelessWidget with SharedDialog {
+  // BiometricStorageFile? _pkStorage;
+
+  SettingPage({Key? key}) : super(key: key);
 
   void _logoutHandler(BuildContext context) async {
     showDialog(
@@ -29,6 +34,25 @@ class SettingPage extends StatelessWidget {
             ));
   }
 
+  // void _biometricHandler(BuildContext context) async {
+  //   final response = await BiometricStorage().canAuthenticate();
+  //   if (response == CanAuthenticateResponse.unsupported) {
+  //     print("rip");
+  //     errDialog(context, "This device do not support biometric");
+  //     return;
+  //   }
+  //   _pkStorage = await BiometricStorage()
+  //       .getStorage('pk', options: StorageFileInitOptions());
+  //   _getPk(_pkStorage!);
+  // }
+
+  // void _getPk(BiometricStorageFile bs) async {
+  //   print("hi");
+  //   bs.write("test");
+  //   String? test = await bs.read();
+  //   print(test);
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -38,6 +62,12 @@ class SettingPage extends StatelessWidget {
             title: Text("Logout"),
             onTap: () {
               _logoutHandler(context);
+            },
+          ),
+          ListTile(
+            title: Text("Use biometric to save private key"),
+            onTap: () {
+              // _biometricHandler(context);
             },
           )
         ],
